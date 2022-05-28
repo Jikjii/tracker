a2c1fe5f29778be7d35b532124130e5acbe9fc2d
Jikjii <85137475+Jikjii@users.noreply.github.com>
2022-05-28 17:56:29 -0400

    semi-completed
    need to add email functionality - will be updated shortly.

--- tracker-app/components/layout/Header.tsx
+++ tracker-app/components/layout/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <Navbar>
             <Container>
-                <NavbarBrand>
+                <NavbarBrand href="#home">
                     Porject Managment
                 </NavbarBrand>
                 <Navbar.Toggle />
@@ -14,7 +14,7 @@ const Header = () => {
                     <Navbar.Text className="p-3">
                         Signed in as: <a href="#login">Mark Otto</a>
                     </Navbar.Text>
-                    <Link href="" passHref>
+                    <Link href="/login" passHref>
                         <Button>Log out</Button>
                     </Link>
                 </Navbar.Collapse>
