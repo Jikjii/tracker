57ec012115493feb94daa7d274b796572f9c5fe3
Jikjii <85137475+Jikjii@users.noreply.github.com>
2022-05-28 16:49:19 -0400

    debugging of add task completed

--- tracker-app/pages/board.tsx
+++ tracker-app/pages/board.tsx
@@ -37,7 +37,8 @@ const Board = () => {
                     let filteredData: Array<Task> = data ? data.tasks.filter((task: Task) => {return task.status === section}) : [];
                     return (
                         <BoardSection
-                        title='Backlog'
+                        title={section}
+                        key={index}
                         tasks={filteredData}
                     />
                 )
